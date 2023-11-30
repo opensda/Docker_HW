c9747b7512881ebc490fc569507875871765b807
Sudarev_D <89365265993@mail.ru>
2023-11-30 18:13:59 +0700

    docker

--- config/settings.py
+++ config/settings.py
@@ -66,6 +66,7 @@ DATABASES = {
         "NAME": os.getenv("DATABASES_NAME"),
         "USER": os.getenv("DATABASES_USER"),
         "PASSWORD": os.getenv("DATABASES_PASSWORD"),
+        "HOST": "db"
     }
 }
 
